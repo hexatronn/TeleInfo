f184808c56c1b693eb6e4a9c72ac69975dc7a49d
hexatronn <98019783+hexatronn@users.noreply.github.com>
2024-08-22 22:00:00 +0400

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,2 +1,6 @@
 API_ID = 'your_api_id'
 API_HASH = 'your_api_hash'
+
+DEFAULT_GROUP = 'my_default_group'
+OUTPUT_FILE = 'output.html'  # Name of the output HTML file
+TEMPLATE_FILE = 'src/templates/index.html'  # Path to the HTML template
